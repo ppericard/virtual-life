50dd7e754dbc83f0153022a9cb8f120559ed8a50
Pierre <pierre.pericard@gmail.com>
2015-02-21 16:28:53 +0100

    tentative de récuperer la vieille version

--- old/VirtualCell.py
+++ old/VirtualCell.py
@@ -9,22 +9,20 @@ class Environment:
 	def __init__(self, height=30, width=50):
 		self.height = height
 		self.width = width
-		self.matrix = [[0 for j in range(self.width)] for i in range(self.height)]
-		self.element_list = list()
+		self.agents_dict = dict()
 		self.__populate()
 	
 	def run_next_step(self):
-		for element in element_list:
-			free_positions = self.__get_free_positions(element.i, element.j)
-			new_position = element.move(free_positions)
-			
+		for (i, j), element in self.agents_dict.items():
+			new_position = element.move(self.get_neighbors(i, j))
+			self.agents_dict[new_position] = self.agents_dict.pop((i, j))
 	
 	def display(self, file_handle=sys.stdout, separator=' '):
 		for i in range(self.height):
 			for j in range(self.width):
 				character = ' '
-				if self.matrix[i][j]:
-					character = self.matrix[i][j].display_character
+				if (i, j) in self.agents_dict:
+					character = self.agents_dict[(i, j)].display_character
 				file_handle.write("{0}{1}".format(character, separator))
 			file_handle.write('\n')
 	
@@ -33,11 +31,17 @@ class Environment:
 			for j in range(self.width):
 				if random.random() < 0.01:
 					element = Cell(i, j)
-					self.matrix[i][j] = element
-					self.element_list.append(element)
+					self.agents_dict[(i, j)] = element
 	
-	def __get_free_positions(self, i, j):
-		pass
+	def get_neighbors(self, i, j):
+		delta = ((-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1))
+		neighbors_dict = dict()
+		for (offset_i, offset_j) in delta:
+			neighbor_position = (i+offset_i, j+offset_j)
+			if neighbor_position in self.agents_dict:
+				neighbors_dict[neighbor_position] = self.agents_dict[neighbor_position]
+		return neighbors_dict
+
 
 class EnvObject:
 	def __init__(self, i, j):
@@ -45,22 +49,22 @@ class EnvObject:
 		self.j = j
 		self.display_character = ' '
 		
-	def move(self, step, i, j, height, width):
-		pass
+	def move(self, neighbors_dict):
+		
 
 class Cell(EnvObject):
 	def __init__(self, i, j):
 		super().__init__(i, j)
 		self.is_living = True
-		self.display_character = 0
+		self.display_character = '0'
 
 if __name__ == '__main__':
 	random.seed()
 	forest = Environment()
-	print(forest.__dict__)
-	while True:
+	for _ in range(100):
 		os.system('clear')
 		forest.display()
+		#~ forest.run_next_step()
 		time.sleep(0.04)
 
 	
