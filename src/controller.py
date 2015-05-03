67a4621a8f0d59ac8b77b7017cabad1ad6487211
Pierre <pierre.pericard@gmail.com>
2015-05-03 23:47:33 +0200

    better code clarity

--- src/controller.py
+++ src/controller.py
@@ -24,20 +24,20 @@ class MyController():
             agent.next_step(self.try_strength)
 
     def start(self):
-        frame_time_start = time.perf_counter()
+        # frame_time_start = time.perf_counter()
         while (True):
-            tmp_time = time.perf_counter()
             os.system('clear')
             self.update_view()
-            frame_time = time.perf_counter() - frame_time_start
-            frame_time_start = time.perf_counter()
+            # frame_time = time.perf_counter() - frame_time_start
+            # frame_time_start = time.perf_counter()
+            sim_time_start = time.perf_counter()
             self.run_next_step()
-            elapsed_time = time.perf_counter() - tmp_time
-            print("sim_time={0} sec".format(elapsed_time))
+            sim_running_time = time.perf_counter() - sim_time_start
+            print("sim_time={0} sec".format(sim_running_time))
             # print("FPS={0}".format(self.view.frame_per_second))
             # print("frame_duration_in_sec={0}".format(self.view.frame_duration_in_sec))
             # print("frame_time={0} sec".format(frame_time))
-            time_left_to_next_frame = self.view.frame_duration_in_sec - elapsed_time
+            time_left_to_next_frame = self.view.frame_duration_in_sec - sim_running_time
             if (time_left_to_next_frame > 0):
                 # print("sleep_time={0}".format(time_left_to_next_frame))
                 time.sleep(time_left_to_next_frame)
